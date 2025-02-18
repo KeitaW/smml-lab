6a84c1c107dd816979482906ff1839d7b3c08509
Keita Watanabe <mlkeita@amazon.com>
2025-02-18 06:56:28 +0000

    fix path

--- ec2/aws-pytorch-conda/0.create_conda_env.sh
+++ ec2/aws-pytorch-conda/0.create_conda_env.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -ex
+set -euxo pipefail
 
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh -b -f -p ./miniconda3
+./Miniconda3-latest-Linux-x86_64.sh -b -f -p ~/miniconda3
 
 source ~/miniconda3/bin/activate
 
