1ae610c3b0244402fd70cb6f1abfaeaf4c2276c1
Keita Watanabe <mlkeita@amazon.com>
2025-05-08 10:29:56 +0900

    remove a stale guide

--- slurm/mount-fsxl-efa/update-efa.sh
+++
@@ -1,7 +0,0 @@
-EFA_INSTALLER_VERSION=1.37.0
-cd $HOME
-curl -O https://efa-installer.amazonaws.com/aws-efa-installer-${EFA_INSTALLER_VERSION}.tar.gz \
-    && tar -xf $HOME/aws-efa-installer-${EFA_INSTALLER_VERSION}.tar.gz \
-    && cd aws-efa-installer \
-    && ./efa_installer.sh -y  \
-    && rm -rf $HOME/aws-efa-installer
--- slurm/mount-fsxl-efa/update-softwares.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update
-sudo apt-get install -y flex bison keyutils libkeyutils-dev libmount-dev libnl-3-dev libnl-genl-3-dev libyaml-dev
-sudo apt-get install -y module-assistant dpatch libselinux-dev quilt linux-headers-generic swig
